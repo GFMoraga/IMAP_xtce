ec01a0badb37d2a4124a6591d6228b74db7570ed
Gabriel M <gabriel.moraga@lasp.colorado.edu>
2023-07-18 11:35:41 -0600

    adding xtce formatted files and code for L0

--- CoDICE_Packets.py
+++ CoDICE_Packets.py
@@ -1,9 +1,20 @@
+''' This the sheet called "Packets" in the provided Excel file. It contains
+    the packet name, APID, extended ID, and max length in bits. The APID is
+    converted to hex and the extended ID is converted to a string. The max
+    length is used to determine the size of the parameter type. The packet name
+    is the specific name of the packet. The extended ID is not given at this time
+    '''
+
 import pandas as pd
-import numpy as np
 import xml.etree.ElementTree as ET
+
 ET.register_namespace('xtce', "http://www.omg.org/space")
 
 # Read the Excel file
+''' This example is pulled from my local, but in the future it will be pulled from the cloud or gateway. 
+    The sheet name is the name of the sheet in the Excel file. The sheet name is used to create the
+    root element. The sheetname can be changed to whatever the user wants.
+    '''
 xls = pd.ExcelFile('/Users/gamo6782/Desktop/COD_TLM_20230316-175906.xlsx')
 sheet_name = xls.sheet_names[1]  # Assuming 'Packets' is the second sheet
 df = xls.parse(sheet_name)
@@ -55,5 +66,3 @@ tree = ET.ElementTree(root)
 ET.indent(tree, space="\t", level=0)
 # Save the XML document to a file
 tree.write('Packets.xml', encoding='utf-8', xml_declaration=True)
-
-
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# Test to see if github is linked
-# Did this work?
-#
+''' This is the main.py file which is used as instructions
+and as a place to test the code before making a new file. It is also a place
+ to make comments about code'''
+
+''' Dependencies: python 3.9 or newer 
+    - pandas
+    - numpy
+    - xml.etree.ElementTree as ET
+    '''
+
+
